398467273f508e77753f78735a7f949d5fea2b2f
KimYongKwon <dydrnjs518@gmail.com>
2020-08-26 17:37:06 +0900

    about
    - about tab fix
    - responsive

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,11 +5,20 @@ function About(props) {
   console.log(props);
   return (
     <div className="about__container">
+      <h1> About Me </h1>
+      <span>This is a project created by Kim Yong-kwon. Thank you!</span>
+      <div className="about__data">
+        <a
+          className="about__link"
+          href="https://github.com/powderBlue91"
+          target="_blank"
+        >
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
       <span>
-        “Freedom is the freedom to say that two plus two make four. If that is
-        granted, all else follows.”
+        <b>Click me!!!</b>
       </span>
-      <span>− George Orwell, 1984</span>
     </div>
   );
 }
